836f50fe8679c7b98ab6292b937b22fe255a00dd
Demian <92870546+Damnndev@users.noreply.github.com>
2024-09-04 17:33:30 +0200

    Create wrapper and add msw

--- src/pages/__test__/ProductsPage.spec.tsx
+++ src/pages/__test__/ProductsPage.spec.tsx
@@ -1,14 +1,43 @@
 import { render, RenderResult, screen } from "@testing-library/react";
 import { ReactNode } from "react";
-import { test } from "vitest";
+import { afterAll, afterEach, beforeAll, describe, test } from "vitest";
 import { AppProvider } from "../../context/AppProvider";
 import { ProductsPage } from "../ProductsPage";
+import { MockWebServer } from "./MockWebServer";
+import productResponse from "./data/productsResponse.json";
 
-test("Loads and displays title", async () => {
-    renderComponent(<ProductsPage />);
+const mockWebServer = new MockWebServer();
 
-    await screen.findAllByRole("heading", { name: "Product price updater" });
+describe("ProductsPage", () => {
+    beforeAll(() => {
+        mockWebServer.start();
+    });
+    afterEach(() => {
+        mockWebServer.resetHandlers();
+    });
+    afterAll(() => {
+        mockWebServer.close();
+    });
+
+    test("Loads and displays title", async () => {
+        givenAProducts();
+
+        renderComponent(<ProductsPage />);
+
+        await screen.findAllByRole("heading", { name: "Product price updater" });
+    });
 });
+
+function givenAProducts() {
+    mockWebServer.addRequestHandlers([
+        {
+            method: "get",
+            endpoint: `https://fakestoreapi.com/products`,
+            httpStatusCode: 200,
+            response: productResponse,
+        },
+    ]);
+}
 
 function renderComponent(component: ReactNode): RenderResult {
     return render(<AppProvider>{component}</AppProvider>);
